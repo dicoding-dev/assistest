5f683d5ea32516dcb49a84e7c3b46994b0a5da49
agis <agis.septiyanto@gmail.com>
2022-10-14 15:57:00 +0700

    create getter

--- src/domain/sever/object/server.ts
+++ src/domain/sever/object/server.ts
@@ -3,18 +3,18 @@ import * as path from 'path';
 import InvariantException from "../../../exception/invariant-exception";
 
 export default class Server {
-    private projectPath: string;
-    private host: string;
-    private port: number;
-    private packageJSONScript: string;
+    private readonly _projectPath: string;
+    private readonly _host: string;
+    private readonly _port: number;
+    private readonly _packageJSONScript: string;
 
     constructor(projectPath: string, host: string, port: number, packageJSONScript: string) {
         this.validate(projectPath, host, port, packageJSONScript)
 
-        this.projectPath = projectPath;
-        this.host = host;
-        this.port = port;
-        this.packageJSONScript = packageJSONScript;
+        this._projectPath = projectPath;
+        this._host = host;
+        this._port = port;
+        this._packageJSONScript = packageJSONScript;
     }
 
     private validate = (projectPath: string, host: string, port: number, packageJSONScript: string) => {
@@ -28,5 +28,17 @@ export default class Server {
         if (!Object.keys(scripts).includes(packageJSONScript)) {
             throw new InvariantException('Runner script not found')
         }
+    }
+    get packageJSONScript(): string {
+        return this._packageJSONScript;
+    }
+    get port(): number {
+        return this._port;
+    }
+    get host(): string {
+        return this._host;
+    }
+    get projectPath(): string {
+        return this._projectPath;
     }
 }
