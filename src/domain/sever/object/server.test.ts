21b9965be303314b5bcecc4c79893385215ffc7b
agis <agis.septiyanto@gmail.com>
2022-10-14 15:49:07 +0700

    create object server test

--- src/domain/sever/object/server.test.ts
+++ src/domain/sever/object/server.test.ts
@@ -2,8 +2,17 @@ import Server from "./server";
 import InvariantException from "../../../exception/invariant-exception";
 
 describe('create server test', () => {
-    it('should throw error when path of project doesn\'t exist', function () {
-        expect(() => createServer('./xxxx')).toThrow(new InvariantException('Path doesn\'t exist'))
+    it('should throw error when project path not contain package.json', function () {
+        expect(() => createServer('./xxxx')).toThrow(new InvariantException('Path not contain package.json'))
+    });
+
+    it('should throw error when runner script not found', function () {
+        expect(() => createServer('.', '', 0, 'invalid-runner')).toThrow(new InvariantException('Runner script not found'))
+    });
+
+    it('should create server properly', function () {
+        const server = createServer('.', '', 0, 'start')
+        expect(server.projectPath).toStrictEqual('.')
     });
 
     const createServer = (
