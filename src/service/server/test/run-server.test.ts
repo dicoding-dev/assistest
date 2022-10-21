400243d00b665ac0ee97c7462413b3a28d07c8e6
agis <agis.septiyanto@gmail.com>
2022-10-21 11:17:12 +0700

    create test if port is used

---
+++ src/service/server/test/run-server.test.ts
@@ -0,0 +1,29 @@
+import RunServer from "../run-server";
+import Server from "../../../domain/sever/object/server";
+import * as kill from 'kill-port';
+import InvariantException from "../../../exception/invariant-exception";
+
+
+describe('run server test', ()=> {
+    afterEach( () => {
+        kill(5000, 'tcp')
+    })
+
+    it('should throw error when port is used', async function () {
+        const port = 5000
+        const server = new Server('./test/student-project/sample-project', 'localhost', port, 'start')
+
+        const runServer = new RunServer()
+
+        //fake server for first server
+        const http = require('http')
+        http.createServer().listen(port)
+
+        // test second sever in same port
+        await expect(runServer.run(server)).rejects.toThrow(new InvariantException(`Port ${port} is used`))
+    });
+
+
+
+
+})
