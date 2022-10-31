ae9580f00577d6f8bb0742ca9f62b1e9ca416b86
agis <agis.septiyanto@gmail.com>
2022-10-31 14:34:27 +0700

    add tree kill as server stopper

--- src/service/server/server-starter.ts
+++ src/service/server/server-starter.ts
@@ -2,31 +2,54 @@ import Server from "../../domain/sever/object/server";
 import {exec} from "child_process";
 import * as tcpPortUsed from 'tcp-port-used';
 import InvariantException from "../../exception/invariant-exception";
+import * as kill from 'tree-kill';
 
 
 class ServerStarter {
+    private _errorLog = [];
+    private serverPort: number;
+    private serverPid: number
+
     async run(server: Server) {
         await this.validateBeforeStart(server)
         const {runnerCommand, projectPath, port} = server
+        this.serverPort = port
         const command = `npm ${runnerCommand}`
         const runningServer = exec(command, {cwd: projectPath});
+        this.serverPid = runningServer.pid
 
         runningServer.stdout.on('data', async (data) => {
-            // console.log(`stdout ${data}`);
+            console.log(`stdout ${data}`);
         });
 
         runningServer.stderr.on('data', (data) => {
-            // console.log(`stderr ${data}`)
+            this._errorLog.push(data)
         });
+
+        runningServer.on('close', () => {
+            console.log('server killed')
+        })
 
         try {
             await tcpPortUsed.waitUntilUsed(port, null, 2000)
-        }catch (e) {
+        } catch (e) {
             throw new Error(`Server cannot started
             message: ${e.message}
             command: ${command}
-            path: ${projectPath}`)
+            path: ${projectPath}
+            hint: Maybe the port server is not 5000`
+            )
         }
+    }
+
+    async stop() {
+        try {
+            kill(this.serverPid)
+            await tcpPortUsed.waitUntilFree(this.serverPort, 500, 4000)
+        } catch (e) {
+            throw new Error(`Failed to kill port ${this.serverPort}, error: ${e}`)
+        }
+
     }
 
       private async validateBeforeStart({port, host}) {
