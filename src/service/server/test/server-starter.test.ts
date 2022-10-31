0cefd0ed5614b8c23e6d6d14233a3d8b868669e2
agis <agis.septiyanto@gmail.com>
2022-10-31 14:43:50 +0700

    add test stop server

--- src/service/server/test/server-starter.test.ts
+++ src/service/server/test/server-starter.test.ts
@@ -1,6 +1,6 @@
 import ServerStarter from "../server-starter";
 import Server from "../../../domain/sever/object/server";
-import * as kill from 'kill-port';
+import * as kill from 'tree-kill';
 import InvariantException from "../../../exception/invariant-exception";
 import axios from "axios";
 
@@ -19,6 +19,27 @@ describe('run server test', () => {
 
         // test second sever in same port
         await expect(serverStarter.run(server)).rejects.toThrow(new InvariantException(`Port ${port} is used`))
+        fakeServer.close()
+    });
+
+    it('should stop server properly', async function () {
+        const port = 5000
+        const host = 'localhost'
+        const server = new Server('./test/student-project/sample-project', host, port, 'start')
+
+        const serverStarter = new ServerStarter()
+        await serverStarter.run(server)
+
+        const response = await axios.get(`http://${host}:${port}`)
+        await expect(response.status).toStrictEqual(200)
+
+        //kill server
+        await serverStarter.stop()
+
+        //validate port 5000 is not used
+        const http = require('http')
+        const fakeServer = http.createServer()
+        fakeServer.listen(port)
         fakeServer.close()
     });
 
