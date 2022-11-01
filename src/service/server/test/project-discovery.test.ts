fa78a5aec69c3f7752d62c3408696c92e1d6ad3a
agis <agis.septiyanto@gmail.com>
2022-11-01 08:57:10 +0700

    create initial test scenario

---
+++ src/service/server/test/project-discovery.test.ts
@@ -0,0 +1,17 @@
+describe('project discovery test', ()=> {
+    it('should found package.json when path is in root folder', function () {
+
+    });
+
+    it('should found package.json when path is in sub folder', function () {
+
+    });
+
+    it('should found package.json when path is in second sub folder', function () {
+
+    });
+
+    it('should not found package.json', function () {
+
+    });
+})
