699997f6131782461f1d269764c98c733227d650
agis <agis.septiyanto@gmail.com>
2022-11-02 10:02:30 +0700

    create sample project for newman runner

---
+++ test/student-project/passed-project/index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const host = 'localhost';
+const port = 5000;
+
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/":
+            res.writeHead(200);
+            res.end("{}");
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({ error: "Resource not found" }));
+    }
+}
+
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
