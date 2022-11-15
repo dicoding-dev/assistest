c190b4d524ff1e9c7cccf3487e46d77419a291bb
agis <agis.septiyanto@gmail.com>
2022-11-15 11:16:20 +0700

    remove dot

--- src/entities/review-result/course-submission-rejection/course-submission-rejection.ts
+++ src/entities/review-result/course-submission-rejection/course-submission-rejection.ts
@@ -64,7 +64,7 @@ class CourseSubmissionRejection {
     private composeRejectionMessageFromServerErrorMessage() {
         const translatedException = exceptionToReviewMessage[this.submissionErrorException.code]
         if (this.submissionErrorException instanceof ServerErrorException){
-            this._messages = `Project yang kamu buat masih belum bisa dijalankan dengan baik, hal ini terjadi karena ${translatedException}. Berikut merupakan log error yang muncul ketika aplikasi dijalankan: ${this.submissionErrorException.serverErrorLog}`
+            this._messages = `Project yang kamu buat masih belum bisa dijalankan dengan baik, hal ini terjadi karena ${translatedException} Berikut merupakan log error yang muncul ketika aplikasi dijalankan: ${this.submissionErrorException.serverErrorLog}`
         }else {
             this._messages = `Project yang kamu buat masih belum bisa dijalankan dengan baik, hal ini terjadi karena ${translatedException}`
         }
