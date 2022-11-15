324f07620b339e17a2c43e5bdcadd3f75a1e88a7
agis <agis.septiyanto@gmail.com>
2022-11-15 11:00:12 +0700

    create function to get command from log error

--- src/service/server/server-error-utils.test.ts
+++ src/service/server/server-error-utils.test.ts
@@ -1,4 +1,4 @@
-import getPortFromLogError from "./server-error-utils";
+import {getCommandFromLogError, getPortFromLogError} from "./server-error-utils";
 
 
 describe('test server utils', () => {
@@ -10,8 +10,22 @@ describe('test server utils', () => {
     });
 
     it('should return null when error log not match port regex', function () {
-        const errorLog = "UnhandledPromiseRejectionWarning: Error: listen EADDRINUSE: address already in use 127.0.0.1 at Server.setupListenHandle [as _listen2] (net.js:1320:16) at listenInCluster (net.js:1368:12) at GetAddrInfoReqWrap.doListen [as callback] (net.js:1505:7) at GetAddrInfoReqWrap.onlookup [as oncomplete] (dns.js:71:8) (Use `node --trace-warnings ...` to show where the warning was created) ,(node:444154) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1) (node:444154) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code."
+        const errorLog = "a error"
         const result = getPortFromLogError(errorLog)
+
+        expect(result).toStrictEqual(null)
+    });
+
+    it('should return nodemon when error log match command error regex', function () {
+        const errorLog = "sh: 1: nodemon: not found"
+        const result = getCommandFromLogError(errorLog)
+
+        expect(result).toStrictEqual('nodemon')
+    });
+
+    it('should return null when error log not match command error regex', function () {
+        const errorLog = "a error"
+        const result = getCommandFromLogError(errorLog)
 
         expect(result).toStrictEqual(null)
     });
--- src/service/server/server-error-utils.ts
+++ src/service/server/server-error-utils.ts
@@ -1,4 +1,4 @@
-const getPortFromLogError = (log: string) => {
+export const getPortFromLogError = (log: string) => {
     const portErrorRegex = new RegExp(/Error: listen EADDRINUSE: address already in use.+?:(\d+)/g)
     const result = portErrorRegex.exec(log)
     if (result !== null) {
@@ -7,5 +7,13 @@ const getPortFromLogError = (log: string) => {
     return null
 }
 
-export default getPortFromLogError
+export const getCommandFromLogError = (log: string) => {
+    const portErrorRegex = new RegExp(/sh: 1: (.+): not found/g)
+    const result = portErrorRegex.exec(log)
+    if (result !== null) {
+        return result[1]
+    }
+    return null
+}
+
 
