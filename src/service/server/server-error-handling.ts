caaa260631763c92ac2e41c702f49b0b3bfb616f
agis <agis.septiyanto@gmail.com>
2022-11-15 13:30:05 +0700

    create server error handler

---
+++ src/service/server/server-error-handling.ts
@@ -0,0 +1,68 @@
+import ProjectErrorException from "../../exception/project-error-exception";
+import ServerErrorException from "../../exception/server-error-exception";
+import SubmissionProject from "../../entities/submission-project/submission-project";
+
+class ServerErrorHandling {
+    private readonly allErrorLog: string;
+    private readonly submissionProject: SubmissionProject;
+    private readonly logErrors: string[];
+
+    constructor(logErrors: string[], submissionProject: SubmissionProject) {
+        this.submissionProject = submissionProject;
+        this.logErrors = logErrors
+        this.allErrorLog = logErrors.join('')
+    }
+
+    throwError() {
+        this.validatePort()
+        this.validateCommand()
+        this.validateModule()
+
+        throw new ServerErrorException(`SERVER_ERROR`,
+            `command: ${this.submissionProject.runnerCommand}
+            path: ${this.submissionProject.projectPath.toString()}
+            hint: Maybe the port server is not 5000`,
+            this.logErrors
+        )
+    }
+
+    private validatePort() {
+        const portFromLogError = this.getPortFromLogError()
+        if (portFromLogError && portFromLogError != this.submissionProject.port) {
+            throw new ProjectErrorException('PORT_NOT_MEET_REQUIREMENT')
+        }
+    }
+
+    private validateCommand() {
+        const commandFromLogError = this.getCommandFromLogError()
+        if (commandFromLogError) {
+            throw new ServerErrorException('COMMAND_NOT_FOUND', null, this.logErrors)
+        }
+    }
+
+    private validateModule() {
+        if (this.allErrorLog.includes('Error: Cannot find module')) {
+            throw new ServerErrorException('MODULE_NOT_FOUND', null, this.logErrors)
+        }
+    }
+
+    private getPortFromLogError() {
+        const portErrorRegex = new RegExp(/Error: listen EADDRINUSE: address already in use.+?:(\d+)/g)
+        const result = portErrorRegex.exec(this.allErrorLog)
+        if (result !== null) {
+            return parseInt(result[1])
+        }
+        return null
+    }
+
+    private getCommandFromLogError() {
+        const portErrorRegex = new RegExp(/sh: 1: (.+): not found/g)
+        const result = portErrorRegex.exec(this.allErrorLog)
+        if (result !== null) {
+            return result[1]
+        }
+        return null
+    }
+}
+
+export default ServerErrorHandling
