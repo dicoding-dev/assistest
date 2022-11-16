7db34fda616846dcf99255c913d4f013d7d03061
agis <agis.septiyanto@gmail.com>
2022-11-16 09:03:16 +0700

    use project error exception

--- src/entities/submission-project/submission-project.unit.test.ts
+++ src/entities/submission-project/submission-project.unit.test.ts
@@ -1,7 +1,7 @@
 import SubmissionProject from "./submission-project";
 import * as fs from "fs";
-import InvariantException from "../../exception/invariant-exception";
 import ProjectPath from "../project-path/project-path";
+import ProjectErrorException from "../../exception/project-error-exception";
 
 jest.mock('fs');
 const mockFS: jest.Mocked<typeof fs> = <jest.Mocked<typeof fs>>fs
@@ -9,28 +9,28 @@ const mockFS: jest.Mocked<typeof fs> = <jest.Mocked<typeof fs>>fs
 describe('create submission project test', () => {
     it('should throw error when project path not contain package.json', function () {
         mockFS.existsSync.mockReturnValue(false)
-        expect(() => createSubmissionProject('./xxxx')).toThrow(new InvariantException('PATH_NOT_CONTAIN_PACKAGE_JSON'))
+        expect(() => createSubmissionProject('./xxxx')).toThrow(new ProjectErrorException('PATH_NOT_CONTAIN_PACKAGE_JSON'))
     });
 
     it('should throw error when package.json cannot be parsed', function () {
         mockFS.existsSync.mockReturnValue(true)
 
         mockFS.readFileSync.mockReturnValue('{ missing curly braces')
-        expect(() => createSubmissionProject('./xxxx')).toThrow(new InvariantException('CANNOT_PARSE_PACKAGE_JSON'))
+        expect(() => createSubmissionProject('./xxxx')).toThrow(new ProjectErrorException('CANNOT_PARSE_PACKAGE_JSON'))
     });
 
     it('should throw error when package.json not contain scripts runner', function () {
         mockFS.existsSync.mockReturnValue(true)
 
         mockFS.readFileSync.mockReturnValue('{ }')
-        expect(() => createSubmissionProject('./xxxx')).toThrow(new InvariantException('PACKAGE_JSON_NOT_CONTAIN_SCRIPT_PROPERTY'))
+        expect(() => createSubmissionProject('./xxxx')).toThrow(new ProjectErrorException('PACKAGE_JSON_NOT_CONTAIN_SCRIPT_PROPERTY'))
     });
 
 
     it('should throw error when runner script not found', function () {
         mockFS.existsSync.mockReturnValue(true)
         mockFS.readFileSync.mockReturnValue('{ "scripts": { "start": "node src/index.js" }}')
-        expect(() => createSubmissionProject('.', '', 0, 'invalid-runner')).toThrow(new InvariantException('RUNNER_SCRIPT_NOT_FOUND'))
+        expect(() => createSubmissionProject('.', '', 0, 'invalid-runner')).toThrow(new ProjectErrorException('RUNNER_SCRIPT_NOT_FOUND'))
     });
 
     it('should create server properly', function () {
