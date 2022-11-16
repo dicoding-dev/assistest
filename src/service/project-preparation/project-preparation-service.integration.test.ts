b6a1abdd590e0299b4b1497ee7d0140784e5c6ac
agis <agis.septiyanto@gmail.com>
2022-11-16 15:05:59 +0700

    refactor to service

---
+++ src/service/project-preparation/project-preparation-service.integration.test.ts
@@ -0,0 +1,39 @@
+import * as fs from "fs";
+import ProjectPreparationService from "./project-preparation-service";
+import SubmissionProject from "../../entities/submission-project/submission-project";
+
+describe('project preparation test', () => {
+    it('should install project properly', function () {
+        const submissionProject = <SubmissionProject>{
+            packageJsonPath: './test/student-project/project-with-dependencies',
+            packageJsonContent: {
+                "name": "project-with-dependencies",
+                "version": "1.0.0",
+                "description": "",
+                "main": "index.js",
+                "scripts": {
+                    "test": "echo \"Error: no test specified\" && exit 1"
+                },
+                "keywords": [],
+                "author": "",
+                "license": "ISC",
+                "dependencies": {
+                    "nanoid": "^4.0.0"
+                }
+            }
+
+        }
+        const projectPreparation = new ProjectPreparationService(submissionProject)
+        projectPreparation.install()
+
+        const isNodeModulesExist = fs.existsSync(`${submissionProject.packageJsonPath}/node_modules`)
+        expect(isNodeModulesExist).toBeTruthy()
+
+        resetTestFile(submissionProject.packageJsonPath)
+    });
+
+    async function resetTestFile(directory: string) {
+        fs.rmSync(`${directory}/node_modules`, {recursive: true, force: true})
+        fs.rmSync(`${directory}/package-lock.json`)
+    }
+})
--- src/service/project-preparation/project-preparation-service.ts
+++ src/service/project-preparation/project-preparation-service.ts
@@ -1,15 +1,16 @@
 import {execSync} from "child_process";
 import SubmissionProject from "../../entities/submission-project/submission-project";
 
-class ProjectPreparation{
+//service
+class ProjectPreparationService {
     private submissionProject: SubmissionProject;
     constructor(submissionProject: SubmissionProject) {
         this.submissionProject = submissionProject;
     }
 
     async install(){
-        execSync('npm install', {cwd: this.submissionProject.projectPath})
+        execSync('npm install', {cwd: this.submissionProject.packageJsonPath})
     }
 }
 
-export default ProjectPreparation
+export default ProjectPreparationService
