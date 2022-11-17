1ad30e17e16c637d5560e568eb413504e49fc050
agis <agis.septiyanto@gmail.com>
2022-11-17 16:32:10 +0700

    simplify flow and remove report generator

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,91 @@
-import {readdirSync, writeFileSync} from "fs";
-import * as path from "path";
-import SubmissionProject from "./entities/submission-project/submission-project";
-import ProjectPath from "./entities/project-path/project-path";
-import ProjectPreparation from "./service/project-preparation/project-preparation";
-import Server from "./service/server/server";
+import PackageJsonFinderService from "./entities/project-path/package-json-finder-service";
+import ProjectPreparationService from "./service/project-preparation/project-preparation-service";
+import ServerService from "./service/server/server-service";
 import PostmanRunner from "./service/postman-runner/postman-runner"
-import * as collection from '../../experiment-storage/postman/collection.json'
-import * as env from '../../experiment-storage/postman/environment.json'
 import EslintChecker from "./service/eslint-checker/eslint-checker";
-import ResultTestFailure from "./service/postman-runner/failure-test";
-import SubmissionCriteriaCheck from "./entities/review-result/submission-criteria-check/submission-criteria-check";
-import backendPemulaChecklist from "./conifg/backend-pemula-checklist";
+import SubmissionCriteriaCheckFactory
+    from "./entities/review-result/submission-criteria-check/submission-criteria-check-factory";
 import ReviewResult from "./entities/review-result/review-result";
 import SubmissionErrorException from "./exception/submission-error-excepion";
 import CourseSubmissionReview from "./entities/review-result/course-submission-review/course-submission-review";
-
-let html = `<table border="1">
-    <tr>
-         <td>Submission</td>
-        <td>Status</td>
-        <td>Rating</td>
-        <td>Message</td>
-        <td>Checklist</td>
-    </tr>`
+import PostmanTestFailedException from "./exception/postman-test-failed-exception";
+import SubmissionProjectFactory from "./entities/submission-project/submission-project-factory";
+import SubmissionProject from "./entities/submission-project/submission-project";
 
 class Main {
-    async main() {
-        const allSubmission = readdirSync('../experiment-storage/project')
-        for (const submission of allSubmission) {
-            const reviewResult = await this.reviewSubmission(submission)
-            this.showReviewResult(reviewResult, submission)
-            writeFileSync('./report/index.html', html)
-        }
-        html += `</table>`
+    private postmanRunner: PostmanRunner;
+    private eslintChecker: EslintChecker;
+    private projectPreparationService: ProjectPreparationService;
+    private serverService: ServerService;
+    private packageJsonFinderService: PackageJsonFinderService;
+    private submissionCriteriaCheckFactory: SubmissionCriteriaCheckFactory
+
+    constructor(
+        postmanRunner: PostmanRunner,
+        serverService: ServerService,
+        projectPreparationService: ProjectPreparationService,
+        eslintChecker: EslintChecker,
+        packageJsonFinderService: PackageJsonFinderService,
+        submissionProjectFactory: SubmissionProjectFactory,
+        submissionCriteriaCheckFactory: SubmissionCriteriaCheckFactory
+    ) {
+        this.submissionProjectFactory = submissionProjectFactory;
+        this.packageJsonFinderService = packageJsonFinderService;
+        this.projectPreparationService = projectPreparationService;
+        this.serverService = serverService;
+        this.postmanRunner = postmanRunner;
+        this.eslintChecker = eslintChecker;
+        this.submissionCriteriaCheckFactory = submissionCriteriaCheckFactory
     }
 
-    private async reviewSubmission(submission: string) {
-        let postmanResult = []
-        let submissionProject: SubmissionProject = null
-        let submissionErrorException: SubmissionErrorException = null
+    public async reviewSubmission(submissionPath: string): Promise<ReviewResult> {
+        let submissionCriteriaCheck = null
         try {
-            console.log(`checking ${submission.toString()}`)
-            submissionProject = this.createSubmissionProject(submission)
-            postmanResult = await this.runServerAndTest(submissionProject)
+            const submissionProject = this.createSubmissionProject(submissionPath)
+            submissionCriteriaCheck = await this.runServerAndTest(submissionProject)
+
+            this.throwErrorIfCriteriaNotFullFilled(submissionCriteriaCheck)
+
+            const eslintCheckResult = this.eslintChecker.check(submissionProject)
+            return this.generateReviewResult(submissionCriteriaCheck, eslintCheckResult)
         } catch (e) {
             if (e instanceof SubmissionErrorException) {
-                submissionErrorException = e
+                submissionCriteriaCheck = submissionCriteriaCheck ?? this.submissionCriteriaCheckFactory.check()
+                return this.generateReviewResult(submissionCriteriaCheck, null, e)
             } else {
                 console.log(e)
             }
         }
-
-        return this.generateReviewResult(submissionProject, postmanResult, submissionErrorException)
     }
 
-    private generateReviewResult(submissionProject, postmanResult, submissionErrorException) {
-        const submissionCriteriaCheck = this.submissionCriteriaCheck(postmanResult, !!submissionErrorException)
-        const eslintCheckResult = this.checkEslint(submissionProject, submissionCriteriaCheck.approvalStatus)
-        const courseSubmissionReview = new CourseSubmissionReview(submissionCriteriaCheck, postmanResult, eslintCheckResult)
-        return courseSubmissionReview.review(submissionErrorException)
+    private throwErrorIfCriteriaNotFullFilled(submissionCriteriaCheck) {
+        if (!submissionCriteriaCheck.approvalStatus) {
+            throw new PostmanTestFailedException('')
+        }
     }
 
-    private showReviewResult(reviewResult: ReviewResult, submission: string) {
-        console.log("status :", reviewResult.status.toString())
-        console.log("rating :", reviewResult.rating)
-        console.log("message :", reviewResult.message)
-        console.log("unfulfilled checklist :", reviewResult.checklist)
-
-        html += `<tr>
-                    <td>${submission}</td>
-                    <td>${reviewResult.status.toString()}</td>
-                    <td>${reviewResult.rating}</td>
-                    <td>${reviewResult.message}</td>
-                    <td>${reviewResult.checklist.filter(checklist => checklist.pass == false).map(checklist => checklist.name).join('<br>')}</td>
-                </tr>`
+    private generateReviewResult(submissionCriteriaCheck, eslintCheckResult?, submissionErrorException?) {
+        const courseSubmissionReview = new CourseSubmissionReview(submissionCriteriaCheck, eslintCheckResult, submissionErrorException)
+        return courseSubmissionReview.review()
     }
 
-    private createSubmissionProject = (submission) => {
-        const submissionPath = path.resolve('../experiment-storage/project', submission)
-        const projectPath = new ProjectPath(submissionPath)
-        return new SubmissionProject(projectPath, 'localhost', 5000, 'start')
-    }
-
-    private prepareSubmissionProject = (submissionProject: SubmissionProject) => {
-        const projectPreparation = new ProjectPreparation(submissionProject)
-        return projectPreparation.install()
-    }
-
-    private startServer = async (submissionProject: SubmissionProject) => {
-        const server = new Server()
-        await server.run(submissionProject)
-        return server
-    }
-
-    private runPostmanTest = async () => {
-        const postmanRunner = new PostmanRunner(collection, env)
-        return postmanRunner.run()
+    private createSubmissionProject = (submissionPath): SubmissionProject => {
+        const projectPath = this.packageJsonFinderService.getPackageJsonDirectory(submissionPath)
+        return this.submissionProjectFactory.create(projectPath)
     }
 
     private runServerAndTest = async (submissionProject: SubmissionProject) => {
-        await this.prepareSubmissionProject(submissionProject)
-        const server = await this.startServer(submissionProject)
-        const postmanResult = await this.runPostmanTest()
-        await server.stop()
+        await this.projectPreparationService.install(submissionProject)
 
-        return postmanResult
+        await this.serverService.run(submissionProject)
+        const postmanResult = await this.postmanRunner.run()
+        await this.serverService.stop()
+
+        return this.submissionCriteriaCheckFactory.check(postmanResult)
     }
 
-    private submissionCriteriaCheck = (failurePostmanTest: Array<ResultTestFailure>, isProjectError: boolean) => {
-        const submissionCriteriaCheck = new SubmissionCriteriaCheck(backendPemulaChecklist, failurePostmanTest, isProjectError)
-        submissionCriteriaCheck.check()
-
-        return submissionCriteriaCheck
-    }
-
-    private checkEslint = (submissionProject: SubmissionProject, approvalStatus: boolean) => {
-        if (!approvalStatus) {
-            return null
-        }
-        const eslintChecker = new EslintChecker(submissionProject)
-        return eslintChecker.check()
-    }
+    private submissionProjectFactory: SubmissionProjectFactory;
 }
 
-new Main().main()
+
+export default Main
