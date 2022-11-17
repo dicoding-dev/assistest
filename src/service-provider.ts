217eae99163356a4272c9ce6af1430fccffc988a
agis <agis.septiyanto@gmail.com>
2022-11-17 17:12:37 +0700

    fix import

--- src/service-provider.ts
+++ src/service-provider.ts
@@ -1,15 +1,14 @@
 import PostmanRunner from "./service/postman-runner/postman-runner";
-import SubmissionProjectFactory from "./entities/submission-project/submission-project-factory";
-import SubmissionCriteriaCheckFactory
-    from "./entities/review-result/submission-criteria-check/submission-criteria-check-factory";
 import backendPemulaChecklist from "./conifg/backend-pemula-checklist";
 import ServerService from "./service/server/server-service";
 import ProjectPreparationService from "./service/project-preparation/project-preparation-service";
 import EslintChecker from "./service/eslint-checker/eslint-checker";
-import PackageJsonFinderService from "./entities/project-path/package-json-finder-service";
 import * as collection from '../../experiment-storage/postman/collection.json'
 import * as env from '../../experiment-storage/postman/environment.json'
 import Main from "./index";
+import SubmissionCriteriaCheckFactory from "./factories/submission-criteria-check/submission-criteria-check-factory";
+import SubmissionProjectFactory from "./factories/submission-project/submission-project-factory";
+import PackageJsonFinderService from "./service/project-path-finder/package-json-finder-service";
 
 const postmanRunner = new PostmanRunner(collection, env)
 const submissionCriteriaCheckFactory = new SubmissionCriteriaCheckFactory(backendPemulaChecklist)
