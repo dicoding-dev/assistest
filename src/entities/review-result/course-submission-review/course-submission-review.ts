94cc99879492f6e861e0666fd781eef2ddd30dae
agis <agis.septiyanto@gmail.com>
2022-11-17 16:25:50 +0700

    treat as aggregate

--- src/entities/review-result/course-submission-review/course-submission-review.ts
+++ src/entities/review-result/course-submission-review/course-submission-review.ts
@@ -1,45 +1,37 @@
-import PostmanTestFailedException from "../../../exception/postman-test-failed-exception";
-import ResultTestFailure from "../../../service/postman-runner/failure-test";
-import SubmissionCriteriaCheck from "../submission-criteria-check/submission-criteria-check";
 import ReviewResult, {ReviewResultStatus} from "../review-result";
 import SubmissionRatingGenerator from "../submission-rating-generator";
 import CourseSubmissionAcception from "../course-submission-acception/course-submission-acception";
 import SubmissionErrorException from "../../../exception/submission-error-excepion";
 import CourseSubmissionRejection from "../course-submission-rejection/course-submission-rejection";
 import EslintCheckResult from "../../../service/eslint-checker/eslint-check-result";
+import SubmissionCriteriaCheck from "../submission-criteria-check/submission-criteria-check";
 
 class CourseSubmissionReview {
     private readonly submissionCriteriaCheck: SubmissionCriteriaCheck;
-    private readonly failurePostmanTest: ResultTestFailure[];
+    private readonly rejectException?: SubmissionErrorException;
     private readonly eslintCheckResult?: EslintCheckResult;
 
     constructor(
         submissionCriteriaCheck: SubmissionCriteriaCheck,
-        failurePostmanTest: ResultTestFailure[],
-        eslintCheckResult?: EslintCheckResult
+        eslintCheckResult?: EslintCheckResult,
+        rejectException?: SubmissionErrorException
     ) {
+        this.rejectException = rejectException;
         this.submissionCriteriaCheck = submissionCriteriaCheck;
-        this.failurePostmanTest = failurePostmanTest;
         this.eslintCheckResult = eslintCheckResult;
     }
 
 
-    review(rejectException?: SubmissionErrorException) {
-        if (rejectException) {
-            return this.generateRejection(rejectException)
+    review() {
+        if (this.rejectException) {
+            return this.generateRejection(this.rejectException)
         }
-
-        if (this.submissionCriteriaCheck.approvalStatus === false) {
-            const e = new PostmanTestFailedException('', this.failurePostmanTest)
-            return this.generateRejection(e)
-        }
-
         return this.generateApproval()
     }
 
 
     private generateApproval(): ReviewResult {
-        const submissionRatingGenerator = new SubmissionRatingGenerator(this.failurePostmanTest, this.eslintCheckResult)
+        const submissionRatingGenerator = new SubmissionRatingGenerator(this.submissionCriteriaCheck.failurePostmanTest, this.eslintCheckResult)
         const courseSubmissionAcception = new CourseSubmissionAcception(this.submissionCriteriaCheck.reviewChecklistResult, submissionRatingGenerator)
         courseSubmissionAcception.accept()
 
@@ -47,12 +39,12 @@ class CourseSubmissionReview {
             rating: courseSubmissionAcception.rating,
             message: courseSubmissionAcception.messages,
             status: ReviewResultStatus.Approve,
-            checklist: courseSubmissionAcception.reviewChecklistResults
+            checklist: this.submissionCriteriaCheck.reviewChecklistResult
         }
     }
 
     private generateRejection(submissionErrorException: SubmissionErrorException): ReviewResult {
-        const courseSubmissionRejection = new CourseSubmissionRejection(submissionErrorException, this.submissionCriteriaCheck.reviewChecklistResult)
+        const courseSubmissionRejection = new CourseSubmissionRejection(submissionErrorException, this.submissionCriteriaCheck)
         courseSubmissionRejection.reject()
 
 
@@ -60,7 +52,7 @@ class CourseSubmissionReview {
             rating: 0,
             message: courseSubmissionRejection.messages,
             status: ReviewResultStatus.Reject,
-            checklist: courseSubmissionRejection.reviewChecklistResults
+            checklist: this.submissionCriteriaCheck.reviewChecklistResult
         }
     }
 }
