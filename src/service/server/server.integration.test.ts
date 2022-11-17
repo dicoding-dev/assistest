01e293c1cac48b3318fc33c59f2a399c779eacfc
agis <agis.septiyanto@gmail.com>
2022-11-17 10:32:08 +0700

    fix test

--- src/service/server/server.integration.test.ts
+++ src/service/server/server.integration.test.ts
@@ -2,93 +2,51 @@ import axios from "axios";
 import * as tcpPortUsed from 'tcp-port-used';
 import Server from "./server";
 import SubmissionProject from "../../entities/submission-project/submission-project";
-import {exec, execSync, spawn, spawnSync} from "child_process";
-import {port} from "../../conifg/backend-pemula-project-requirement";
-import * as killPort from 'kill-port'
+import {exec, execSync} from "child_process";
 import * as kill from "tree-kill";
+import PackageJson from "../../entities/submission-project/package-json";
 
 describe('run server test', () => {
-    jest.setTimeout(100000)
-    // afterEach(async () => {
-    //     // restore the spy created with spyOn
-    //     jest.restoreAllMocks();
-    //
-    //     try {
-    //
-    //
-    //     tcpPortUsed.check(port, '127.0.0.1')
-    //         .then(function(inUse) {
-    //             console.log(`Port ${port}} usage: `+inUse);
-    //             if (inUse){
-    //                 console.log('stopping server')
-    //                 const ls = spawn(`kill`, ['-9 $(lsof -t -i:${5000})'])
-    //
-    //
-    //                 ls.stdout.on('data', (data) => {
-    //                     console.log(`stdout: ${data}`);
-    //                 });
-    //
-    //                 ls.stderr.on('data', (data) => {
-    //                     console.error(`stderr: ${data}`);
-    //                 });
-    //
-    //                 ls.on('close', (code) => {
-    //                     console.log(`child process exited with code ${code}`);
-    //                 });
-    //
-    //
-    //                 console.log('done stopping server')
-    //             }
-    //         }, function(err) {
-    //             console.error('Error on check:', err.message);
-    //         });
-    //
-    //
-    //     const a = await tcpPortUsed.waitUntilFree(port, null, 10000)
-    //     console.log(a)
-    //     }catch (e) {
-    //         console.log(e)
-    //     }
-    //
-    //
-    //     // await new Promise(resolve => setTimeout(resolve, 1000))
-    //     console.log('done stopping server')
-    // });
+    afterEach(async () => {
+        if (await isPortUsed(5000)){
+            await killPort(5000)
+        }
+    });
 
 
     it.skip('should start & stop server', async function () {
-        for (let i = 0; i <10; i++) {
+        for (let i = 0; i < 10; i++) {
             const port = 5000
             await startFakeServer(port)
 
-            expect(await isPortUsed(port, 'first')).toBeTruthy()
+            expect(await isPortUsed(port)).toBeTruthy()
             await killPort(port)
-            expect(await isPortUsed(port, "second")).toBeFalsy()
+            expect(await isPortUsed(port)).toBeFalsy()
         }
     });
 
     it('should throw error when port is used', async function () {
         const port = 5000
-        const submissionProject = new SubmissionProject(new ProjectPath('test/student-project/sample-project'), 'localhost', port, 'start')
+        const submissionProject: SubmissionProject = {
+            packageJsonPath: 'test/student-project/sample-project',
+            packageJsonContent: <PackageJson>{}
+        }
+
 
         const server = new Server()
 
         //fake server for first server
-        const http = require('http')
-        const fakeServer = http.createServer()
-        fakeServer.listen(port)
+        await startFakeServer(port)
 
         // test second sever in same port
         await expect(server.run(submissionProject)).rejects.toThrow(new Error(`Port ${port} is not available`))
-        fakeServer.close()
     });
 
     it('should throw error and stop server when port is not used after project running', async function () {
-        const wrongPort = 9999
-        const host = 'localhost'
-        // real project port is 5000
-        const realPort = 5000
-        const submissionProject = new SubmissionProject(new ProjectPath('./test/student-project/sample-project'), host, wrongPort, 'start')
+        const submissionProject: SubmissionProject = {
+            packageJsonPath: 'test/student-project/project-with-bad-port',
+            packageJsonContent: <PackageJson>{}
+        }
 
         const server = new Server()
 
@@ -98,13 +56,17 @@ describe('run server test', () => {
         await expect(spy).toBeCalled()
 
         //wait real port to close
-        await tcpPortUsed.waitUntilFree(realPort, null, 2000)
+        await tcpPortUsed.waitUntilFree(5000, null, 2000)
     });
+
 
     it('should stop server properly', async function () {
         const port = 5000
         const host = 'localhost'
-        const submissionProject = new SubmissionProject(new ProjectPath('./test/student-project/sample-project'), host, port, 'start')
+        const submissionProject:SubmissionProject = {
+            packageJsonPath: 'test/student-project/sample-project',
+            packageJsonContent: <PackageJson>{}
+        }
 
         const server = new Server()
         await server.run(submissionProject)
@@ -114,30 +76,26 @@ describe('run server test', () => {
 
         //kill server
         await server.stop()
-
-        //validate port 5000 is not used
-        const http = require('http')
-        const fakeServer = http.createServer()
-        fakeServer.listen(port)
-        fakeServer.close()
+        expect(await isPortUsed(5000)).toBeFalsy()
     });
-
+    //
     it('should run server properly', async function () {
         const port = 5000
         const host = 'localhost'
-        const submissionProject = new SubmissionProject(new ProjectPath('test/student-project/sample-project'), host, port, 'start')
+        const submissionProject:SubmissionProject = {
+            packageJsonPath: 'test/student-project/sample-project',
+            packageJsonContent: <PackageJson>{}
+        }
 
         const server = new Server()
         await expect(server.run(submissionProject)).resolves.not.toThrow()
 
         const response = await axios.get(`http://${host}:${port}`)
         await expect(response.status).toStrictEqual(200)
-
-        await server.stop()
     });
 
     async function startFakeServer(port) {
-        exec('node index.js', {
+        exec(`PORT=${port} node index.js`, {
             cwd: './test/student-project/simple-server',
         })
 
@@ -148,11 +106,10 @@ describe('run server test', () => {
         }
     }
 
-    async function isPortUsed(port, message: string) {
+    async function isPortUsed(port) {
         return tcpPortUsed.check(port, '127.0.0.1').then((inUse) => Promise.resolve(inUse)
             , function (e) {
                 console.log(e)
-                console.log(message)
                 throw new Error('Cannot check port')
             });
     }
