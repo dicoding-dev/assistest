bddb9be3ef265720269572fd1f69b1061f50d334
agis <agis.septiyanto@gmail.com>
2022-11-17 16:56:42 +0700

    merge interface

--- src/entities/review-result/submission-criteria-check/submission-criteria-check.ts
+++ src/entities/review-result/submission-criteria-check/submission-criteria-check.ts
@@ -1,10 +1,17 @@
-import ReviewChecklistResult from "../review-checklist-result";
 import ResultTestFailure from "../../../service/postman-runner/failure-test";
 
 interface SubmissionCriteriaCheck {
     reviewChecklistResult: Array<ReviewChecklistResult>
     failurePostmanTest: Array<ResultTestFailure>;
     approvalStatus: boolean
+}
+
+
+export interface ReviewChecklistResult {
+    name: string,
+    pass: boolean,
+    requirement: Array<string>,
+    reason?: Array<ResultTestFailure>
 }
 
 export default SubmissionCriteriaCheck
