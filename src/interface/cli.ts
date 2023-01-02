e95da8ab0fc5142a20705b6563bc37e401b8bbcd
agis <agis.septiyanto@gmail.com>
2023-01-02 14:55:04 +0700

    refactor

--- src/interface/cli.ts
+++ src/interface/cli.ts
@@ -1,39 +1,56 @@
 import {main} from "../service-provider";
 import {readdirSync} from "fs";
-
-console.log(process.argv)
-
-const folderPath = process.argv.find(arg => arg.includes('path=')).replace('path=', '')
-const mode = process.argv.find(arg => arg.includes('mode=')).replace('mode=', '') ?? 'single'
-
 import ReportGenerator from "../service/report-generator/report-generator";
 import * as path from "path";
 
-console.time('run')
+class Cli {
+    private mode: string;
+    private reportPath: string;
+    private folderPath: string;
 
-async function run() {
-    const reportGenerator = new ReportGenerator()
-
-    if (mode === 'multi') {
-        const allSubmission = readdirSync(folderPath)
-        for (const submission of allSubmission) {
-            // if (!submission.includes('1572925 christiantokape')) continue
-            console.log(`checking ${submission}`)
-            const submissionPath = path.resolve(folderPath, submission)
-            const reviewResult = await main.reviewSubmission(submissionPath)
-            reportGenerator.generate(reviewResult, submission)
-            console.log()
-        }
-    } else {
-        const submissionPath = path.resolve(folderPath)
-        const reviewResult = await main.reviewSubmission(submissionPath)
-        reportGenerator.generate(reviewResult, submissionPath)
+    constructor() {
+        this.getInput()
+        this.validateInput()
     }
 
-    console.timeEnd('run')
-    process.exit()
+    async run() {
+        const reportGenerator = new ReportGenerator(this.reportPath)
 
+        if (this.mode === 'multi') {
+            const allSubmission = readdirSync(this.folderPath)
+            for (const submission of allSubmission) {
+                console.log(`checking ${submission}`)
+                const submissionPath = path.resolve(this.folderPath, submission)
+                const reviewResult = await main.reviewSubmission(submissionPath)
+                reportGenerator.generate(reviewResult, submission)
+                console.log()
+            }
+        } else {
+            const submissionPath = path.resolve(this.folderPath)
+            const reviewResult = await main.reviewSubmission(submissionPath)
+            reportGenerator.generate(reviewResult, submissionPath)
+        }
+    }
+
+    private validateInput() {
+        if (!this.reportPath) {
+            throw Error('reportPath must not empty')
+        }
+
+        if (!this.folderPath){
+            throw Error('Submission source is not specified yet')
+        }
+    }
+
+    private getInput() {
+        this.folderPath = process.argv.find(arg => arg.includes('path='))?.replace('path=', '')
+        this.mode = process.argv.find(arg => arg.includes('mode=')).replace('mode=', '') ?? 'single'
+        this.reportPath = process.argv.find(arg => arg.includes('reportPath='))?.replace('reportPath=', '')
+    }
 }
 
+console.time('run')
+new Cli().run().then(()=> {
+    console.timeEnd('run')
+})
 
-run()
