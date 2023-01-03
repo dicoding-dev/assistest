e893cc6b6d279335d989ef90f844c816eb878fca
agis <agis.septiyanto@gmail.com>
2023-01-03 15:01:36 +0700

    catch if error happen

--- src/service/project-preparation/project-preparation-service.ts
+++ src/service/project-preparation/project-preparation-service.ts
@@ -1,9 +1,17 @@
 import {execSync} from "child_process";
 import SubmissionProject from "../../entities/submission-project/submission-project";
+import ServerErrorException from "../../exception/server-error-exception";
 
 class ProjectPreparationService {
-    async install(submissionProject: SubmissionProject){
-        execSync('npm install', {cwd: submissionProject.packageJsonPath})
+    install(submissionProject: SubmissionProject) {
+        try {
+            execSync('npm install', {cwd: submissionProject.packageJsonPath, stdio: 'pipe', encoding:'utf-8'})
+        } catch (e) {
+            const errorLog = e.stderr
+                .replace(/npm ERR! A complete log of this r.*/gs, '')
+                .trim()
+            throw new ServerErrorException('FAIL_INSTALLING_PACKAGE', '', errorLog)
+        }
     }
 }
 
