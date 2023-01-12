8c1564abf7c2f74cd57ba2f40f9ca3932483a316
agis <agis.septiyanto@gmail.com>
2023-01-12 14:12:39 +0700

    use new submission requirement

--- src/service-provider.ts
+++ src/service-provider.ts
@@ -1,17 +1,17 @@
 import PostmanRunner from "./service/postman-runner/postman-runner";
-import postmanRequirements from "./config/postman-requirements";
 import ServerService from "./service/server/server-service";
 import ProjectPreparationService from "./service/project-preparation/project-preparation-service";
 import EslintChecker from "./service/eslint-checker/eslint-checker";
-import * as collection from './config/collection.json'
-import * as env from './config/environment.json'
+import * as collection from './config/postman/postman-cli/collection.json'
+import * as env from './config/postman/postman-cli/environment.json'
 import Main from "./index";
 import SubmissionCriteriaCheckFactory from "./factories/submission-criteria-check/submission-criteria-check-factory";
 import SubmissionProjectFactory from "./factories/submission-project/submission-project-factory";
 import PackageJsonFinderService from "./service/project-path-finder/package-json-finder-service";
+import getSubmissionRequirement from "./config/submission-requirement";
 
 const postmanRunner = new PostmanRunner(collection, env)
-const submissionCriteriaCheckFactory = new SubmissionCriteriaCheckFactory(postmanRequirements)
+const submissionCriteriaCheckFactory = new SubmissionCriteriaCheckFactory(getSubmissionRequirement())
 const submissionProjectFactory = new SubmissionProjectFactory()
 const serverService = new ServerService()
 const projectPreparationService = new ProjectPreparationService()
