96c5db8348f88aac9b2d72de5039b8f1d04e5d3a
agis <agis.septiyanto@gmail.com>
2023-01-18 11:38:50 +0700

    add review template

--- src/service/report-generator/report-generator.ts
+++ src/service/report-generator/report-generator.ts
@@ -1,6 +1,7 @@
 import ReviewResult, {ReviewResultStatus} from "../../entities/review-result/course-submission-review/review-result";
 import {writeFileSync} from "fs";
 import * as fs from "fs";
+import ReviewTemplateGenerator from "../review-template-generator/review-template-generator";
 
 
 class ReportGenerator {
@@ -13,6 +14,7 @@ class ReportGenerator {
 
     generate(reviewResult: ReviewResult, submissionPath: string) {
         const isApproved = reviewResult.status === ReviewResultStatus.Approve
+        const reviewTemplateGenerator = new ReviewTemplateGenerator(submissionPath)
 
         const completedChecklist = Object.keys(reviewResult.checklist)
             .filter(checklistName => reviewResult.checklist[checklistName].status === true && reviewResult.checklist[checklistName].checklistId)
@@ -22,7 +24,7 @@ class ReportGenerator {
             review_id: Date.now(),
             is_approved: isApproved,
             rating: reviewResult.rating,
-            failure: reviewResult.message,
+            message: reviewTemplateGenerator.getReviewMessageWithTemplate(reviewResult.message, isApproved),
             submission_path: submissionPath,
             checklist: reviewResult.checklist,
             completed_checklist: completedChecklist
