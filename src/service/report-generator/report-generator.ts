18060f345be53db9d33bec7457b77df493489df2
agis <agis.septiyanto@gmail.com>
2023-01-19 13:21:43 +0700

    move template and checklist resolver to here

--- src/service/report-generator/report-generator.ts
+++ src/service/report-generator/report-generator.ts
@@ -1,8 +1,8 @@
 import ReviewResult, {ReviewResultStatus} from "../../entities/review-result/course-submission-review/review-result";
-import {writeFileSync} from "fs";
 import * as fs from "fs";
+import {existsSync, readFileSync, writeFileSync} from "fs";
 import ReviewTemplateGenerator from "../review-template-generator/review-template-generator";
-
+import * as templates from "../../config/review-template.json";
 
 class ReportGenerator {
     private readonly reportPath: string;
@@ -14,26 +14,77 @@ class ReportGenerator {
 
     generate(reviewResult: ReviewResult, submissionPath: string) {
         const isApproved = reviewResult.status === ReviewResultStatus.Approve
-        const reviewTemplateGenerator = new ReviewTemplateGenerator(submissionPath)
 
-        const completedChecklist = Object.keys(reviewResult.checklist)
-            .filter(checklistName => reviewResult.checklist[checklistName].status === true && reviewResult.checklist[checklistName].checklistId)
-            .map(checklistName => reviewResult.checklist[checklistName].checklistId)
+        const autoReviewConfig = this.getAutoReviewConfig(submissionPath)
 
         const summary = {
+            submission_id: autoReviewConfig?.id,
             review_id: Date.now(),
             is_approved: isApproved,
             rating: reviewResult.rating,
-            message: reviewTemplateGenerator.getReviewMessageWithTemplate(reviewResult.message, isApproved),
+            message: this.getReviewMessageWithTemplate(reviewResult, autoReviewConfig),
             submission_path: submissionPath,
             checklist: reviewResult.checklist,
-            completed_checklist: completedChecklist
+            completed_checklist: this.getCompletedChecklist(reviewResult, autoReviewConfig)
         };
 
         this.result.push(summary);
         fs.mkdirSync(this.reportPath, {recursive: true});
 
         writeFileSync(`${this.reportPath}/report.json`, JSON.stringify(this.result))
+    }
+
+    private getCompletedChecklist(reviewResult: ReviewResult, autoReviewConfig){
+        if (!autoReviewConfig){
+            return []
+        }
+
+        Object.keys(reviewResult.checklist).forEach(requirementName => {
+            const requirement = reviewResult.checklist[requirementName]
+            requirement.checklistId = requirement.possibleChecklistId
+                .find(checklistId =>  autoReviewConfig.checklist.includes(checklistId)) ?? null
+        })
+
+        return  Object.keys(reviewResult.checklist)
+            .filter(checklistName => reviewResult.checklist[checklistName].status === true && reviewResult.checklist[checklistName].checklistId)
+            .map(checklistName => reviewResult.checklist[checklistName].checklistId)
+    }
+
+    getReviewMessageWithTemplate(reviewResult: ReviewResult, autoReviewConfig) {
+        const mainTemplate = templates.find(template => template.courseId === autoReviewConfig?.course_id)
+
+        if (!mainTemplate) {
+            return reviewResult.message
+        }
+
+        let template: string
+        if (reviewResult.status === ReviewResultStatus.Approve) {
+            template = mainTemplate.approvalTemplate
+        } else {
+            template = mainTemplate.rejectionTemplate
+        }
+
+        return template
+            .replace('$submitter_name', autoReviewConfig.submitter)
+            .replace('$review_message', reviewResult.message)
+    }
+
+
+    private getAutoReviewConfig(projectPath: string): any|null {
+
+        const configFilePath = `${projectPath}/autoreview-config.json`
+        if (!existsSync(configFilePath)) {
+            return
+        }
+        const getConfigFile = (configFilePath: string) => {
+            try {
+                return JSON.parse(readFileSync(configFilePath).toString())
+            } catch (e) {
+                return null
+            }
+        }
+
+        return getConfigFile(configFilePath)
     }
 }
 
