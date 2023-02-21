0a148456a356e28649201dc7e5c182a4fa9a90c3
agis <agis.septiyanto@gmail.com>
2023-02-21 14:18:41 +0700

    update review message

--- src/exception/exception-to-review-message.ts
+++ src/exception/exception-to-review-message.ts
@@ -1,5 +1,5 @@
 const exceptionToReviewMessage = {
-    'PATH_NOT_CONTAIN_PACKAGE_JSON': 'kami tidak bisa menemukan file package.json pada submission yang kamu kirimkan, perlu diingat pada umumnya aplikasi node js memiliki file package.json untuk menyimpan konfigurasi filenya.',
+    'PATH_NOT_CONTAIN_PACKAGE_JSON': 'kami tidak bisa menemukan file package.json pada submission yang kamu kirimkan, perlu diingat pada umumnya aplikasi node js memiliki file package.json untuk menyimpan konfigurasi project.',
     'PACKAGE_JSON_NOT_CONTAIN_SCRIPT_PROPERTY': 'file package.json yang kamu buat masih belum memiliki properti "script". Properti script ini digunakan sebagai shortcut untuk menjalankan command. Silakan sesuaikan isi script dengan kriteria submission ya',
     'RUNNER_SCRIPT_NOT_FOUND': 'sepertinya kamu lupa memasukkan command untuk menjalankan project ke dalam properti "script" pada file package.json. Silakan ditambahkan terlebih dahulu yaa.',
     'CANNOT_PARSE_PACKAGE_JSON': 'sepertinya file package.json yang kamu buat belum bisa terformat dengan baik. Silakan diperbaiki yaa.',
