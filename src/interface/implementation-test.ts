8382b40be268e7e04e5720af4bad5d888de3df51
Dimas Saputra <dimas.maulanads@gmail.com>
2023-09-14 16:47:41 +0700

    improve pathing

--- src/interface/implementation-test.ts
+++ src/interface/implementation-test.ts
@@ -1,17 +1,17 @@
 import {readdirSync} from "fs";
 import {join} from "path";
-import * as path from "path";
 import {main} from "../service-provider";
 import ReportGenerator from "../service/report-generator/report-generator";
 
-const reportGenerator = new ReportGenerator('./report')
 async function run() {
-    const allSubmission = readdirSync(join(process.cwd(), 'experiment-storage', 'project'))
+    const experimentStorageProjectPath = join(process.cwd(), 'experiment-storage', 'project')
+    const allSubmission = readdirSync(experimentStorageProjectPath)
 
     for (const submission of allSubmission) {
         console.log(`checking ${submission}`)
-        const submissionPath = path.resolve('../experiment-storage/project', submission)
+        const submissionPath = join(experimentStorageProjectPath, submission)
         const reviewResult = await main.reviewSubmission(submissionPath)
+        const reportGenerator = new ReportGenerator(submissionPath)
         reportGenerator.generate(reviewResult, submission)
     }
 }
