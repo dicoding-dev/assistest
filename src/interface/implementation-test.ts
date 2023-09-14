41249f00cfa2df415e31abca4571198d932d2a35
Dimas Saputra <dimas.maulanads@gmail.com>
2023-09-14 16:30:43 +0700

    restructure experiment-storage

--- src/interface/implementation-test.ts
+++ src/interface/implementation-test.ts
@@ -1,11 +1,12 @@
 import {readdirSync} from "fs";
+import {join} from "path";
 import * as path from "path";
 import {main} from "../service-provider";
 import ReportGenerator from "../service/report-generator/report-generator";
 
 const reportGenerator = new ReportGenerator('./report')
 async function run() {
-    const allSubmission = readdirSync('../experiment-storage/project')
+    const allSubmission = readdirSync(join(process.cwd(), 'experiment-storage', 'project'))
 
     for (const submission of allSubmission) {
         console.log(`checking ${submission}`)
