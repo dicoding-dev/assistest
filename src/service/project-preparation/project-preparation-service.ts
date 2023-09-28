a055b323e131b98d25f4cbe3906960298eb45e1b
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2023-09-28 19:40:01 +0700

    Revert "Improvement/remove lock support after given write permission by grader runner"

--- src/service/project-preparation/project-preparation-service.ts
+++ src/service/project-preparation/project-preparation-service.ts
@@ -2,10 +2,27 @@ import {execSync} from "child_process";
 import SubmissionProject from "../../entities/submission-project/submission-project";
 import ServerErrorException from "../../exception/server-error-exception";
 import raiseDomainEvent from "../../common/domain-event";
+import * as fs from "fs";
+import {join} from "path";
 
 class ProjectPreparationService {
     install(submissionProject: SubmissionProject) {
         try {
+            const isPackageLockExist = fs.existsSync(join(submissionProject.packageJsonPath, 'package-lock.json'))
+            const isYarnLockExist = fs.existsSync(join(submissionProject.packageJsonPath, 'yarn.lock'))
+
+            if (isPackageLockExist) {
+                execSync('npm ci', {cwd: submissionProject.packageJsonPath, stdio: 'pipe', encoding:'utf-8'})
+                raiseDomainEvent('dependencies installed')
+                return
+            }
+
+            if (isYarnLockExist) {
+                execSync('yarn install --frozen-lockfile', {cwd: submissionProject.packageJsonPath, stdio: 'pipe', encoding:'utf-8'})
+                raiseDomainEvent('dependencies installed')
+                return
+            }
+
             execSync('npm install', {cwd: submissionProject.packageJsonPath, stdio: 'pipe', encoding:'utf-8'})
             raiseDomainEvent('dependencies installed')
         } catch (e) {
